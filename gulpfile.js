cbb6953b75a34add3b0f561eb20fb1355862e8df
Johannes Rubenz <design@rubenz.at>
2015-09-09 13:50:19 +0200

    Fixes a severe problem with my gulp-streams (some tasks without return-statements!)

--- gulpfile.js
+++ gulpfile.js
@@ -37,28 +37,28 @@ var AUTOPREFIXER_BROWSERS = [
   'bb >= 10'
 ];
 
-//error handling
-function errorAlert(error){
+// Error handling
+function errorAlert(error) {
 
   var lineNumber = (error.lineNumber) ? 'LINE ' + error.lineNumber + ': ' : '';
   if (error.fileName) {
     var filename = (error.fileName).split('/');
-    filename =  filename[filename.length -1];
+    filename = filename[filename.length - 1];
   } else {
     filename = '';
   }
 
   notify({
-    subtitle:  error.plugin.toUpperCase() + ' caused an error.',
+    subtitle: error.plugin.toUpperCase() + ' caused an error.',
     title: lineNumber + filename,
     sound: 'Glass' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
   }).write(error);
 
   // Inspect the error object
-  //console.log(error);
+  // console.log(error);
 
   // Easy error reporting
-  //console.log(error.toString());
+  // console.log(error.toString());
 
   // Pretty error reporting
   var report = '';
@@ -66,8 +66,12 @@ function errorAlert(error){
 
   report += chalk('TASK:') + ' [' + error.plugin + ']\n';
   report += chalk('PROB:') + ' ' + error.message + '\n';
-  if (error.lineNumber) { report += chalk('LINE:') + ' ' + error.lineNumber + '\n'; }
-  if (error.fileName)   { report += chalk('FILE:') + ' ' + error.fileName + '\n'; }
+  if (error.lineNumber) {
+    report += chalk('LINE:') + ' ' + error.lineNumber + '\n';
+  }
+  if (error.fileName) {
+    report += chalk('FILE:') + ' ' + error.fileName + '\n';
+  }
   console.error(report);
 
   // Prevent the 'watch' task from stopping
@@ -75,96 +79,128 @@ function errorAlert(error){
 };
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function () {
-browserSync({
-server: src
-});
-gulp.watch(src + "/sass/**/*.scss", ['sass']);
-gulp.watch([src + "/js/**/*.js", notjssrc], ['js']);
+gulp.task('serve', ['sass'], function() {
+  browserSync({
+    server: src
+  });
+  gulp.watch(src + "/sass/**/*.scss", ['sass']);
+  gulp.watch([src + "/js/**/*.js", notjssrc], ['js']);
   gulp.watch(src + "/*.html").on('change', reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function () {
-  return gulp.src(src +  "/sass/*.scss")
-    .pipe(plumber({errorHandler: errorAlert}))
+gulp.task('sass', function() {
+  return gulp.src(src + "/sass/*.scss")
+    .pipe(plumber({
+      errorHandler: errorAlert
+    }))
     .pipe(sass())
-    .pipe(autoprefixer({browsers: ['chrome >= 40']})) // no autprefixing needed in development
+    .pipe(autoprefixer({
+      browsers: ['chrome >= 40']
+    })) // no autprefixing needed in development
     .pipe(gulp.dest(src))
-    .pipe(reload({stream: true}));
-});
-
-// Lint JS first, then concat and minify
-gulp.task('js', ['jslint'], function () {
-  return gulp.src([src + '/js/lib/*.js', src + '/js/*.js', notjssrc])
-         .pipe(concat('main.js'))
-         .pipe(gulp.dest(src + '/js/'))
-         .pipe(reload({stream: true}));
+    .pipe(reload({
+      stream: true
+    }));
 });
 
 // JSlint
-gulp.task('jslint', function () {
-return gulp.src([src + '/js/*.js', notjssrc])
-.pipe(jshint())
-.pipe(jshint.reporter('jshint-stylish'));
+gulp.task('jslint', function() {
+  return gulp.src([src + '/js/*.js', notjssrc])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// Lint JS first, then concat and minify
+gulp.task('js', ['jslint'], function() {
+  return gulp.src([src + '/js/lib/*.js', src + '/js/*.js', notjssrc])  .pipe(concat('main.js'))    .pipe(gulp.dest(src + '/js/'))
+    .pipe(reload({
+      stream: true
+    }));
 });
 
 // compress images
-gulp.task('compress', function () {
-return gulp.src(src + '/images/**')
-.pipe(imagemin({
-progressive: true,
-interlaced: true,
-svgoPlugins: [{removeViewBox: false}],
-use: [pngquant()]
-}))
-.pipe(gulp.dest(dest + '/images/'));
+gulp.task('compress', function() {
+  return gulp.src(src + '/images/**')
+    .pipe(imagemin({
+      progressive: true,
+      interlaced: true,
+      svgoPlugins: [{
+        removeViewBox: false
+      }],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest(dest + '/images/'));
 });
 
-var production = function () {
-process.stdout.write("\nTo minify and copy images, run the 'compress'-task.\n\n");
-    // compress CSS
-    gulp.src(src + "/sass/*.scss")
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+// PRODUCTION
+gulp.task('sass-prod', function() {
+  return gulp.src(src + "/sass/*.scss")
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(autoprefixer({
+      browsers: AUTOPREFIXER_BROWSERS
+    }))
     .pipe(gulp.dest(dest));
-    // uglify JS 
-    gulp.src(src + '/js/main.js')
+});
+
+gulp.task('js-prod', ['jslint'], function() {
+  // uglify JS 
+  return gulp.src(src + '/js/main.js')
     .pipe(uglify())
-        .pipe(gulp.dest(dest + '/js/'));
-    // copy index.html, favicon.ico and touch-icon-images
-    gulp.src([src + '/*', "!" + src + "/style.css", "!" + src + "/sass"])
+    .pipe(gulp.dest(dest + '/js/'));
+});
+
+gulp.task('copy-files-root', function() {
+  // but do not copy the dev's style.css and the sass folder
+  return gulp.src([src + '/*', "!" + src + "/style.css", "!" + src + "/sass"])
+    .pipe(changed(dest))
     .pipe(gulp.dest(dest));
-    gulp.src(src + '/images/icons-touch/*')
+});
+
+gulp.task('copy-files', function() {
+  // copy touch-icon-images
+  return gulp.src(src + '/images/icons-touch/*')
     .pipe(changed(dest + '/images/icons-touch'))
     .pipe(gulp.dest(dest + '/images/icons-touch'));
-    };
+});
 
 // Preparing files for production: First, run the 'js' task, afterwards ...
-gulp.task('production' ,['js'], production);
+gulp.task('production', ['js-prod', 'sass-prod', 'copy-files-root', 'copy-files'], function() {
+  process.stdout.write("\nTo minify and copy images, run the 'compress'-task.\n\n");
+});
+
 // alias task name
-gulp.task('prod',['js'], production);
+gulp.task('prod', ['js-prod', 'sass-prod', 'copy-files-root', 'copy-files'], function() {
+  process.stdout.write("\nTo minify and copy images, run the 'compress'-task.\n\n");
+});
 
 // Clean Output Directory
-gulp.task('clear', del.bind(null, [dest + '/**/*'], {dot: true}));
+gulp.task('clear', del.bind(null, [dest + '/**/*'], {
+  dot: true
+}));
 
 // Google pagespeed
-gulp.task('pagespeed', function (cb) {
-// Update the below URL to the public URL of your site
-pagespeed.output('example.com', {
-strategy: 'mobile'
-// By default we use the PageSpeed Insights free (no API key) tier.
-// Use a Google Developer API key if you have one: http://goo.gl/RkN0vE
-// key: 'YOUR_API_KEY'
-}, cb);
+gulp.task('pagespeed', function(cb) {
+  // Update the below URL to the public URL of your site
+  pagespeed.output('example.com', {
+    strategy: 'mobile'
+      // By default we use the PageSpeed Insights free (no API key) tier.
+      // Use a Google Developer API key if you have one: http://goo.gl/RkN0vE
+      // key: 'YOUR_API_KEY'
+  }, cb);
 });
 
 // Easy FTP-upload
 gulp.task('deploy', ['prod'], function() {
   var conn = ftp.create(ftpconfig);
-  return gulp.src( './build/index.html', { base: 'build', buffer: false } )
-  .pipe( conn.newer( '/www' ) ) // only upload newer files
-  .pipe( conn.dest( '/www' ) );
+  return gulp.src('./build/index.html', {
+      base: 'build',
+      buffer: false
+    })
+    .pipe(conn.newer('/www')) // only upload newer files
+    .pipe(conn.dest('/www'));
 });
 
 // Default: turn the server on and refresh/inject on change!
