e52fa4a064c176c0191db2e8ca7b0a01ec9ffdf9
Johannes Rubenz <design@rubenz.at>
2017-03-16 13:35:25 +0100

    Update to v3.0

--- README.md
+++ README.md
@@ -1,57 +1,59 @@
 # jsidiots-gulpfile
-**A gulpfile.js for JS idiots (like me)**
+## A gulpfile.js for regular webdevelopers
 
+### What's it all about?
+If you don't consider yourself a rockstar, a ninja, or a 10X kind of person:
+good news! Here is a gulpfile that simply works, is easy to understand and makes
+developing a new site a breeze. It comes with some convenient conventions and
+defaults to make your webdev life easier.
+ 
+### Version 3.0
+#### Reduce to the max!
+* updates all dependencies to the latest versions (as of 2017-03-16).
+* gets rid of more exoctic gulp taks
 
-This is a gulpfile that simply works, is easy to understand and aimed at webdevelopers starting a new website project - without the fancy stuff (yes, I am talking about you, Browserifiers).
-
-**How to use**
-* install the prerequisites: [NPM](https://www.npmjs.com/), [gulp](http://gulpjs.com/)
+### How to use
+* if you haven't already, install [npm](https://www.npmjs.com/).
 * clone this respository to your local machine
 * `npm install`
-* (get some coffee and ignore the many npm warnings)
-* run any of the gulp commands listed below, eg. `gulp` or `gulp production`
+* run any of the gulp commands listed below, eg. `gulp` or `gulp prod`
 
-**What it does for you**
-* uses sass (supporting the scss-syntax) and minifies your css for production
-* autoprefixes your css-file depending on your choice of browsers you want to support
-* autorefreshes browsers and brower-synchronisation
+### Features
+* uses sass (supporting the scss-syntax), minifies and auto-prefixes your css for production.
+* auto-prefixing depending on your choice of browsers you want to support, using
+    caniuse-statistics
+* autorefreshes browsers (autoreloads) (requires the [Chrome live-reload
+    extension](https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei))
 * concatenates, minifies and lints your js-files
 * creates all the files needed for production in the build directory
 * minifies images (jpg, png, jpg, svg)
-* tests the pagespeed of your site
 * includes various icon references for touch devices and a favicon.ico as a reminder
 * uploads the production files that have been changed to your server
-* If there is a syntax error in your sass code, gulp serve/watch won't crash (but an error message will
-  be displayed instead [using gulp-plumber]).
 
-**directory structure**
+### folder structure
 
-`/src`: put all your code and images here. Be careful not to put code into 'src/js/main.js' (s. below).
-
+`/src`: put all your code and images here. Be careful not to put code into `src/js/main.js` (s. below).  
 `/build`: will contain all the files ready for production. To automatically generate them, use `gulp prod`.
 
 You can easily change the names of your source and build folder on the first two lines of gulpfile.js according to your own preferences.
 
+### gulp commands
+* `gulp` or `gulp serve`: starts the server (using browsersync so that you can easily test your sites on all devices available). Refreshes the browsers automatically when you edit and save your scss, js or index.html files. Use this command while working on your project.
+* `gulp compress`: minifies your images and copies them to the production (build) folder
+* `gulp prod`:  generates and copies all the files you need for deployment into the build folder
 
-**gulp commands**
-* `gulp`: starts the server (using browsersync so that you can easily test your sites on all devices available). Refreshes the browsers automatically when you edit and save your scss, js or index.html files. Use this command while working on your project.
-* `gulp clear`: deletes all files in your build folder
-* `gulp pagespeed`: tests the speed of your site with google page speed (make sure to edit the name of your live site in your gulpfile)
-* `gulp lint` checks your JS-code (and is invoked every time you change any of your JS-files)
-* `gulp prod` or `gulp production`: generates and copies all the files you need for deployment into the build folder
-* `gulp deploy`: prepares files for production and uploads them to your server
-  using the config in ftpconfig.js 
-
-**JS files**
-
+### JS files
 All JS-files in your 'src/js' folder get concatenated into 'src/js/main.js'. JS-files in 'src/js/lib' will appear first.
 
-**Wanna contribute?**
+### Wanna contribute?
+Cool, thanks! But keep in mind: I will most likely not add tasks and features in order to keep things simple. But there is always room for improvement, so get in touch!
 
-I would highly appreciate your contributions. Especially, if you could please help me to make the gulpfile more readable and more quality code (read: real JS). New tasks and features, however, will most likely not be added in order to keep things simple (and in line with the repository's intention/name).
+### Credits
 
-**Credits**
+The idiot's gulpfile is based on Google's [web-starter-kit](https://github.com/google/web-starter-kit) and [greypants' gulp starter](https://github.com/greypants/gulp-starter).
 
-The idiot's gulpfile is based on Google's [web-starter-kit](https://github.com/google/web-starter-kit) and [greypants' gulp starter](https://github.com/greypants/gulp-starter). Thank you for your great work and inspiration!
+### What else?
+Follow me on [twitter](https://twitter.com/j_rubenz)!
 
-
+### Is that all?
+Yep.
--- gulpfile.js
+++ gulpfile.js
@@ -1,154 +1,75 @@
-var dest      = "./build",
-    src       = './src',
-    notjssrc  = '!' + src + '/js/main.js';
+var src = './src'
+var dest = './build'
+var notjssrc  = '!' + src + '/js/main.js';
 
-var ftpconfig = require('./ftpconfig');
-
-var gulp         = require('gulp');
-var browserSync  = require('browser-sync');
-var sass         = require('gulp-sass');
-var reload       = browserSync.reload;
-var concat       = require('gulp-concat');
-var uglify       = require('gulp-uglify');
-var imagemin     = require('gulp-imagemin');
-var pngquant     = require('imagemin-pngquant');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var pagespeed    = require('psi');
-var del          = require('del');
-var jshint       = require('gulp-jshint');
-var stylish      = require('jshint-stylish');
-var plumber      = require('gulp-plumber');
-var notify       = require('gulp-notify');
-var growl        = require('growl');
-var gutil        = require('gulp-util');
-var ftp          = require('vinyl-ftp');
-var changed      = require('gulp-changed');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var browserSync = require('browser-sync').create();
+var changed = require('gulp-changed');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
-var AUTOPREFIXER_BROWSERS = [
-  'ie >= 8',
-  'ie_mob >= 8',
-  'ff >= 30',
-  'chrome >= 34',
-  'safari >= 7',
-  'opera >= 23',
-  'ios >= 7',
-  'android >= 2.1',
-  'bb >= 10'
-];
-
-// Error handling
-function errorAlert(error) {
-
-  var lineNumber = (error.lineNumber) ? 'LINE ' + error.lineNumber + ': ' : '';
-  if (error.fileName) {
-    var filename = (error.fileName).split('/');
-    filename = filename[filename.length - 1];
-  } else {
-    filename = '';
-  }
-
-  notify({
-    subtitle: error.plugin.toUpperCase() + ' caused an error.',
-    title: lineNumber + filename,
-    sound: 'Glass' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
-  }).write(error);
-
-  // Inspect the error object
-  // console.log(error);
-
-  // Easy error reporting
-  // console.log(error.toString());
-
-  // Pretty error reporting
-  var report = '';
-  var chalk = gutil.colors.white.bgRed;
-
-  report += chalk('TASK:') + ' [' + error.plugin + ']\n';
-  report += chalk('PROB:') + ' ' + error.message + '\n';
-  if (error.lineNumber) {
-    report += chalk('LINE:') + ' ' + error.lineNumber + '\n';
-  }
-  if (error.fileName) {
-    report += chalk('FILE:') + ' ' + error.fileName + '\n';
-  }
-  console.error(report);
-
-  // Prevent the 'watch' task from stopping
-  this.emit('end');
-};
-
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-  browserSync({
-    server: src
-  });
-  gulp.watch(src + "/sass/**/*.scss", ['sass']);
-  gulp.watch([src + "/js/**/*.js", notjssrc], ['js']);
-  gulp.watch(src + "/*.html").on('change', reload);
-});
-
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-  return gulp.src(src + "/sass/*.scss")
-    .pipe(plumber({
-      errorHandler: errorAlert
-    }))
-    .pipe(sass())
-    .pipe(autoprefixer({
-      browsers: ['chrome >= 40']
-    })) // no autprefixing needed in development
+// SASS to CSS
+gulp.task('sass', function () {
+  return gulp.src(src + '/sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest(src))
-    .pipe(reload({
-      stream: true
-    }));
+    .pipe(browserSync.stream());
 });
 
-// JSlint
-gulp.task('jslint', function() {
-  return gulp.src([src + '/js/*.js', notjssrc])
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
-});
-
-// Lint JS first, then concat and minify
-gulp.task('js', ['jslint'], function() {
-  return gulp.src([src + '/js/lib/*.js', src + '/js/*.js', notjssrc])  .pipe(concat('main.js'))    .pipe(gulp.dest(src + '/js/'))
-    .pipe(reload({
-      stream: true
-    }));
-});
-
-// compress images
-gulp.task('compress', function() {
-  return gulp.src(src + '/images/**')
-    .pipe(imagemin({
-      progressive: true,
-      interlaced: true,
-      svgoPlugins: [{
-        removeViewBox: false
-      }],
-      use: [pngquant()]
-    }))
-    .pipe(gulp.dest(dest + '/images/'));
-});
-
-// PRODUCTION
-gulp.task('sass-prod', function() {
-  return gulp.src(src + "/sass/*.scss")
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
+// SASS to minified and prefixed CSS (prefixing according to browser statistics)
+gulp.task('sass-prod', function () {
+  return gulp.src(src + '/sass/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer({
-      browsers: AUTOPREFIXER_BROWSERS
+      browsers: ['> 5%'],
+      cascade: false
     }))
     .pipe(gulp.dest(dest));
 });
 
+// concat js, lib folder files come first
+gulp.task('js', ['jslint'], function() {
+  return gulp.src([src + '/js/lib/*.js', src + '/js/*.js', notjssrc])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(src + '/js/'))
+    .pipe(browserSync.stream());
+});
+
+// minify js
 gulp.task('js-prod', ['jslint'], function() {
   // uglify JS 
   return gulp.src(src + '/js/main.js')
     .pipe(uglify())
     .pipe(gulp.dest(dest + '/js/'));
+});
+
+// lint your js
+gulp.task('jslint', function() {
+  return gulp.src([src + '/js/*.js', notjssrc])
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+// start a server for static content + watch scss/html files
+// make sure you have the chrome livereload plugin installed (s. http://livereload.com/)
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server: src
+  });
+  gulp.watch(src + "/sass/**/*.scss", ['sass']);
+  gulp.watch([src + "/js/**/*.js", notjssrc], ['js']);
+  gulp.watch(src + "/*.html").on('change', browserSync.reload);
+});
+
+// preparing files for production
+gulp.task('prod', ['js-prod', 'sass-prod', 'copy-files-root', 'copy-files'], function() {
+  console.log("\nTo minify and copy images, run the 'compress'-task.\n\n");
 });
 
 gulp.task('copy-files-root', function() {
@@ -165,42 +86,18 @@ gulp.task('copy-files', function() {
     .pipe(gulp.dest(dest + '/images/icons-touch'));
 });
 
-// Preparing files for production: First, run the 'js' task, afterwards ...
-gulp.task('production', ['js-prod', 'sass-prod', 'copy-files-root', 'copy-files'], function() {
-  process.stdout.write("\nTo minify and copy images, run the 'compress'-task.\n\n");
+// compress images
+gulp.task('compress', function() {
+  return gulp.src(src + '/images/**')
+    .pipe(imagemin({
+      progressive: true,
+      interlaced: true,
+      svgoPlugins: [{
+        removeViewBox: false
+      }],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest(dest + '/images/'));
 });
 
-// alias task name
-gulp.task('prod', ['js-prod', 'sass-prod', 'copy-files-root', 'copy-files'], function() {
-  process.stdout.write("\nTo minify and copy images, run the 'compress'-task.\n\n");
-});
-
-// Clean Output Directory
-gulp.task('clear', del.bind(null, [dest + '/**/*'], {
-  dot: true
-}));
-
-// Google pagespeed
-gulp.task('pagespeed', function(cb) {
-  // Update the below URL to the public URL of your site
-  pagespeed.output('example.com', {
-    strategy: 'mobile'
-      // By default we use the PageSpeed Insights free (no API key) tier.
-      // Use a Google Developer API key if you have one: http://goo.gl/RkN0vE
-      // key: 'YOUR_API_KEY'
-  }, cb);
-});
-
-// Easy FTP-upload
-gulp.task('deploy', ['prod'], function() {
-  var conn = ftp.create(ftpconfig);
-  return gulp.src('./build/**/*', {
-      base: 'build',
-      buffer: false
-    })
-    .pipe(conn.newer('/')) // only upload newer files
-    .pipe(conn.dest('/'));
-});
-
-// Default: turn the server on and refresh/inject on change!
 gulp.task('default', ['serve']);
